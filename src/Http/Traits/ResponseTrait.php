f97f64cef3cb3632acbc9545569f1aeaa210e862
zengweitao <zengweitao@msn.com>
2023-01-29 17:10:42 +0800

    init

--- src/Http/Traits/ResponseTrait.php
+++ src/Http/Traits/ResponseTrait.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Baichuan\Library\Http\Trait;
+namespace Baichuan\Library\Http\Traits;
 
 use Baichuan\Library\Http\Resource\JsonResource;
 use Baichuan\Library\Http\Resource\ResourceCollection;
