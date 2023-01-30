12f6c6ab7bf93991f9ae93eb3537cfd2a4a5f405
zengweitao <zengweitao@msn.com>
2023-01-30 14:25:43 +0800

    init

---
+++ src/Component/Config/ConfigProvider.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @see     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ *
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace Baichuan\Library\Component\Config;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => [
+                //PingServiceInterface::class => PingService::class,
+            ],
+            'processes' => [
+            ],
+            'commands' => [
+            ],
+            'aspects' => [
+            ],
+            'listeners' => [
+            ],
+            'annotations' => [
+                'scan' => [
+                    'collectors' => [
+                        //ErrorCodeCollector::class,
+                        //WsMiddlewareAnnotationCollector::class,
+                    ],
+                    'paths' => [
+                        //__DIR__,
+                    ],
+                    'class_map' => [
+                        // 需映射的類名 => 類所在的文件路徑
+                        //\Hyperf\Amqp\ConsumerManager::class => __DIR__ . '/class_map/Hyperf/Amqp/ConsumerManager.php',
+                        //\Hyperf\SocketIOServer\Emitter\Emitter::class => __DIR__ . '/class_map/Hyperf/SocketIOServer/Emitter/Emitter.php',
+                        //\Mix\Redis\Subscribe\Subscriber::class => __DIR__ . '/class_map/Mix/Redis/Subscribe/Subscriber.php',
+                    ],
+                ],
+            ],
+            'publish' => [
+                [
+                    'id' => 'config',
+                    'description' => '備註信息1',
+                    'source' => __DIR__ . '/Template/example1.php',
+                    'destination' => BASE_PATH . '/config/autoload/example1.php',
+                ],
+                [
+                    'id' => 'config',
+                    'description' => '備註信息2',
+                    'source' => __DIR__ . '/Template/example2.php',
+                    'destination' => BASE_PATH . '/config/autoload/example2.php',
+                ],
+            ],
+            'macros' => [
+            ],
+        ];
+    }
+}
