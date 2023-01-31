3b2962361e47a80ac91d423630650523632bae78
zengweitao <zengweitao@msn.com>
2023-01-31 18:14:33 +0800

    init

--- src/Component/Logger/TraceHandler.php
+++ src/Component/Logger/TraceHandler.php
@@ -33,6 +33,7 @@ class TraceHandler
     public static function __callStatic($function, $argument)
     {
         [$message, $label, $context, $name, $group] = $argument + ['', '', [], '', 'default'];
+        xdebug([$message, $label, $context, $name, $group],'222');
         $logger = static::get($name, $group);
         $logger->{$function}(self::customFormat($message, $label), $context);
     }
