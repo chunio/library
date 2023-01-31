a10a90bbf0b1516fcc6a87530957ce3579f8fb73
zengweitao <zengweitao@msn.com>
2023-01-31 17:57:03 +0800

    init

--- src/Component/Logger/StdoutLoggerFactory.php
+++ src/Component/Logger/StdoutLoggerFactory.php
@@ -17,6 +17,6 @@ class StdoutLoggerFactory
 {
     public function __invoke(ContainerInterface $container)
     {
-        return Log::get('sys');
+        return TraceHandler::get('sys');
     }
 }
