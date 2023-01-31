13111f92b22d48dcfeb507a5c5e98614a9a3cf6b
zengweitao <zengweitao@msn.com>
2023-01-31 10:36:15 +0800

    init

--- src/Aspect/RequestAspect.php
+++ src/Aspect/RequestAspect.php
@@ -43,6 +43,7 @@ class RequestAspect extends AbstractAspect
         if ($proceedingJoinPoint->className == 'Hyperf\HttpServer\CoreMiddleware' && $proceedingJoinPoint->methodName == 'dispatch') {
             $res = $proceedingJoinPoint->process();
             $request = Context::get(ServerRequestInterface::class);
+            xdebug($request,'$request0');
             $this->logRequest($request);
             return $res;
         }
@@ -51,6 +52,7 @@ class RequestAspect extends AbstractAspect
         if ($proceedingJoinPoint->className == 'Hyperf\HttpServer\ResponseEmitter' && $proceedingJoinPoint->methodName == 'emit') {
             $request = Context::get(ServerRequestInterface::class);
             $response = $proceedingJoinPoint->getArguments()[0];
+            xdebug($response,'$response0');
             $this->logResponse($request, $response);
             return $res;
         }
@@ -102,7 +104,7 @@ class RequestAspect extends AbstractAspect
             $isJson = false !== strpos($response->getHeaderLine('Content-Type'), 'application/json');
             $json = $isJson ? json_decode($body, true) : [];
 
-            $body = $isJson ? prettyJsonEncode($json) : '内容格式是: ' . $response->getHeaderLine('Content-Type');
+            $body = $isJson ? prettyJsonEncode($json) : 'Content-Type : ' . $response->getHeaderLine('Content-Type');
             $body = $this->trimByMaxLength('response', $body);
 
             $responseInfo = [
