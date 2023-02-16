bd929b7661f1f388d54c3ace338c16aea10d2738
zengweitao <zengweitao@msn.com>
2023-02-16 16:20:21 +0800

    init

--- src/Http/Resource/JsonResource.php
+++ src/Http/Resource/JsonResource.php
@@ -40,7 +40,7 @@ class JsonResource extends \Hyperf\Resource\Json\JsonResource
     public function toArray(): array
     {
         if (is_null($this->resource) || is_string($this->resource) || /*is_numeric($this->resource) ||*/ is_bool($this->resource)) {
-            return [];
+            return 'xxxxxx';//[];
         }
 
         return is_array($this->resource)
