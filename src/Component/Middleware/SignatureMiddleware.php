89532bebdb88321caba6cf868ec3dc3d3c97c25e
zengweitao <zengweitao@msn.com>
2023-02-21 21:05:21 +0800

    init

---
+++ src/Component/Middleware/SignatureMiddleware.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Component\Hyperf\Http\Middleware;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Hyperf\Context\Context;
+
+class SignatureMiddleware implements MiddlewareInterface
+{//
+
+    /**
+     * @var ContainerInterface
+     */
+    protected ContainerInterface $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        //signature[START]
+        /**
+         * app : d5fe18479296485de713003a28f54154d0105392d8407e6291bb4449faf79bdf
+         * cms : x1fe18479296485de713003a28f54154816k5392d8407e6291bb4449faf79bdu
+         */
+        if(1){//button
+            $currentTime = time();
+            $auth = $request->getHeaderLine('auth');
+            if(!$auth) {
+                throw new Exception('hacker attack (0)');
+            }
+            try{
+                $jwtArray = explode('.', $auth);
+                [$payloadBase64, $tokenBase64, $signatureBase64] = $jwtArray;
+            }catch (\Throwable $e){
+                throw new Exception("hacker attack ({$auth} is incorrect)");
+            }
+            //DEBUG_LABEL[START]
+            if(in_array(env('APP_ENV'), ['local', 'sandox', 'test', 'official'/*TODO:待優化*/])){
+                if($signatureBase64 === 'd5fe18479296485de713003a28f54154d0105392d8407e6291bb4449faf79bdf'){
+                    if(substr($tokenBase64,0,8) === 'payload='){//
+                        [$field, $payloadJson] = explode('=', $tokenBase64);
+                        $payload = json_decode($payloadJson,true);
+                        //特殊字符轉換[START]
+                        if(isset($payload['version'])){
+                            $payload['version'] = str_replace('#', '.',  $payload['version']);
+                        }
+                        if(isset($payload['package_name'])){
+                            $payload['package_name'] = str_replace('#', '.',  $payload['package_name']);
+                        }
+                        //特殊字符轉換[END]
+                        return $payload;
+                    }
+                    return [//default
+                        'client_id' => 10000,
+                        'device_id' => 'sandbox0000000000000000000000001',
+                        'device_type' => 'postman'
+                    ];
+                }
+                if($signatureBase64 === 'x1fe18479296485de713003a28f54154816k5392d8407e6291bb4449faf79bdu') return ['client_id' => 10001];
+            }
+            //DEBUG_LABEL[END]
+            $payload = json_decode(base64_decode($payloadBase64),true);
+            $token = base64_decode($tokenBase64);
+            $clientSignature = base64_decode($signatureBase64);
+            if(
+            !isset(
+                $payload['client_id'],//1
+                $payload['device_id'],
+                $payload['device_type'],//2
+                $payload['device_model'],
+                $payload['timestamp'],
+                $payload['version'],
+                $payload['build_number'],
+                $payload['package_name']//2
+            )
+            ) {
+                throw new Exception('hacker attack (1)');
+            }
+            //format[START]
+            $payload['client_id'] = intval($payload['client_id']);
+            $payload['timestamp'] = intval($payload['timestamp']);
+            //format[END]
+            if(($currentTime - $payload['timestamp']) > config('system.middleware.signature.timeout',7200)) {
+                $code = env('APP_ENV') == 'prod' ? 401 : ErrorCodeSys::AUTH_TIMEOUT;//DEBUG_LABEL
+                throw new Exception("timestamp error ( {$payload['timestamp']} {$currentTime} )", $code);
+            }
+            $parameter = array_merge($request->getParsedBody(), $request->getQueryParams());
+            $secret = config("system.middleware.signature.client.{$payload['client_id']}.secret");
+            //step0：併入{auth_token && timestamp}
+            $tempStruct = array_merge($payload, ['token' => $token], $parameter);
+            //step1：排除{空值||數組}
+            foreach ($tempStruct as $key => $value) {
+                if ((!$value && $value !== 0 && $value !== '0') || is_array($value)) unset($tempStruct[$key]);
+            }
+            //step2：參數鍵名降序排序
+            ksort($tempStruct);
+            //$serverSignature = strtolower(md5(http_build_query($tempStruct) . $secret));
+            $serverSignature = strtolower(md5(urlencode(http_build_query($tempStruct)) . $secret));
+            //$serverSignature = strtolower(md5(str_replace('\\', '', json_encode($tempStruct, JSON_UNESCAPED_SLASHES/*|JSON_UNESCAPED_UNICODE*/)) . $secret));
+            //xdebug(json_encode($tempStruct, JSON_UNESCAPED_SLASHES/*|JSON_UNESCAPED_UNICODE*/),'json encode');
+            //xdebug(str_replace('\\', '', json_encode($tempStruct, JSON_UNESCAPED_SLASHES/*|JSON_UNESCAPED_UNICODE*/)),'str_replace json encode');
+            $alarm = [
+                '$payload' => $payload,
+                '$token' => $token,
+                '$clientSignature' => $clientSignature,
+                '$secret' => $secret,
+                '$tempStruct' => $tempStruct,
+                '$serverSignature' => $serverSignature,
+                'httpBuildQuery' => http_build_query($tempStruct),
+                'urlencode' => urlencode(http_build_query($tempStruct)),
+                //'json_encode()' => str_replace('\\', '', json_encode($tempStruct, JSON_UNESCAPED_SLASHES/*|JSON_UNESCAPED_UNICODE*/)),
+            ];
+            //step3：http_build_query >> md5 >> strtolower
+            if($clientSignature != $serverSignature) {
+                throw new Exception('hacker attack (2)');
+            }
+        }
+        Context::set('ryenovelPayload', $payload ?? []);
+        //signature[END]
+        return $handler->handle($request);
+    }
+
+}
