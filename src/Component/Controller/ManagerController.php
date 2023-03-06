b4259f5b7f5eba2f4ea067258083bcf141d87777
zengweitao <zengweitao@msn.com>
2023-03-06 16:36:51 +0800

    init

--- src/Component/Controller/ManagerController.php
+++ src/Component/Controller/ManagerController.php
@@ -15,17 +15,18 @@ use Hyperf\HttpServer\Router\Router;
 class ManagerController extends AbstractController
 {
 
-    public static function addRoutes()
-    {
-        Router::addRoute(['GET', 'POST'], '/manager', [self::class, 'api_rank']);
-    }
+//    public static function addRoutes()
+//    {
+//        Router::addRoute(['GET', 'POST'], '/manager', [self::class, 'api_rank']);
+//    }
+
     /**
-     * @RequestMapping(path="api_rank", methods="get,post")
+     * @RequestMapping(path="index3", methods="get,post")
      */
     public function ApiRank()
     {
         make(ManagerLogic::class)->apiRank();
-        return $this->success(1);
+        return $this->success(3);
     }
 
 }
