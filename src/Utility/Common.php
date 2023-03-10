6b4645d5b04049d94b6138adf7b9fbe44ab2ae58
zengweitao <zengweitao@msn.com>
2023-03-10 10:36:48 +0800

    init

--- src/Utility/Common.php
+++ src/Utility/Common.php
@@ -10,27 +10,27 @@ use Baichuan\Library\Handler\UtilityHandler;
 use Hyperf\Kafka\ProducerManager;
 use Hyperf\Redis\RedisFactory;
 
-if (!function_exists('traceHandler')) {
-    function traceHandler($variable, string $label = '', string $level = 'info', $monolog = true): bool
-    {
-        if($monolog && class_exists(MonologHandler::class)){
-            MonologHandler::$level($variable, $label);
-        }else{
-            //非協程I/O[START]
-            $path = BASE_PATH . "/runtime/logs/" . __FUNCTION__ . "-0000-00-" . date("d") . ".log";//keep it for one month
-            if (!file_exists($path)) touch($path);//compatible file_put_contents() cannot be created automatically
-            $trace = TraceHandler::traceFormatter($variable, $label);
-            if (abs(filesize($path)) > 1024 * 1024 * 1024) {//flush beyond the limit/1024m
-                file_put_contents($path, $trace/*, LOCK_EX*/); //TODO:阻塞風險
-            } else {
-                file_put_contents($path, $trace, FILE_APPEND/* | LOCK_EX*/);
-            }
-            if(UtilityHandler::matchEnvi('local')) echo "$trace\n";
-            //非協程I/O[END]
-        }
-        return true;
-    }
-}
+//if (!function_exists('traceHandler')) {
+//    function traceHandler($variable, string $label = '', string $level = 'info', $monolog = true): bool
+//    {
+//        if($monolog && class_exists(MonologHandler::class)){
+//            MonologHandler::$level($variable, $label);
+//        }else{
+//            //非協程I/O[START]
+//            $path = BASE_PATH . "/runtime/logs/" . __FUNCTION__ . "-0000-00-" . date("d") . ".log";//keep it for one month
+//            if (!file_exists($path)) touch($path);//compatible file_put_contents() cannot be created automatically
+//            $trace = TraceHandler::traceFormatter($variable, $label);
+//            if (abs(filesize($path)) > 1024 * 1024 * 1024) {//flush beyond the limit/1024m
+//                file_put_contents($path, $trace/*, LOCK_EX*/); //TODO:阻塞風險
+//            } else {
+//                file_put_contents($path, $trace, FILE_APPEND/* | LOCK_EX*/);
+//            }
+//            if(UtilityHandler::matchEnvi('local')) echo "$trace\n";
+//            //非協程I/O[END]
+//        }
+//        return true;
+//    }
+//}
 
 if (!function_exists('modelHandler')) {
     function modelHandler(string $model): ModelHandler
