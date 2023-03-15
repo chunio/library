05a1416b582a34c069068cda56b2339e68e50abe
zengweitao <zengweitao@msn.com>
2023-03-15 10:49:50 +0800

    init

--- src/Handler/MongoDBHandler.php
+++ src/Handler/MongoDBHandler.php
@@ -50,7 +50,7 @@ class MongoDBHandler
         //$this->MongoClient->database()->collection($collection);
     }
 
-    public function one(array $where, array $select = [], array $order = []): array
+    public function one(array $where, array $select = [], array $group = [], array $order = []): array
     {
         //format option[START]
         $option = [];
@@ -80,45 +80,44 @@ class MongoDBHandler
      * datetime: 2023/02/23 14:13
      * memo : $where僅支持邏輯與
      */
-    public function commonList(array $where, array $select = [], array $group = []/*預留*/, array $order = []): array
+    public function commonList(array $where, array $select = [], array $group = [], array $order = []): array
     {
-        //format option[START]
-        $option = [];
-        if($select){
-            $id = false;
-            foreach ($select as $unitField){
-                if($unitField === '_id') $id = true;
-                $option['projection'/*聲明需返回的字段*/][$unitField] = 1;//1表示返回
-            }
-            if(!$id) $option['projection']['_id'] = 0;//因爲_id默認返回
+        $option = self::formatOption($select, $order);
+        if($group){
+            $group = array_map(fn ($v) => '$' . $v, $group);
+            return $this->MongoClient->database($this->db)->collection($this->collection)->aggregate([
+                [
+                    '$group' =>
+                        [
+                            '_id' => $group,
+                            'count' => ['$sum' => 1]
+                        ]
+                ],
+            ], $option);
+        }else{
+            $where = self::formatWhere($where);
+            return $this->MongoClient->database($this->db)->collection($this->collection)->find($where, $option);
         }
-        if($order){
-            foreach ($order as $unitField => $unitSequence){
-                $option['sort'][$unitField] = ($unitSequence === 'ASC') ? 1/*正序*/ : -1/*倒敘*/;
-            }
-        }
-        //format option[END]
-        return $this->MongoClient->database($this->db)->collection($this->collection)->find(self::formatWhere($where), $option);
     }
 
-    /**
-     * @param string $field
-     * author : zengweitao@gmail.com
-     * datetime: 2023/02/23 20:59
-     * memo : 以$field分組，並統計各每組成員總數
-     */
-    public function aggregate(string $field): array
-    {
-        return $this->MongoClient->database($this->db)->collection($this->collection)->aggregate([
-            [
-                '$group' =>
-                    [
-                        '_id' => $field,
-                        'count' => ['$sum' => 1]
-                    ]
-            ],
-        ]);
-    }
+//    /**
+//     * @param string $field
+//     * author : zengweitao@gmail.com
+//     * datetime: 2023/02/23 20:59
+//     * memo : 以$field分組，並統計各每組成員總數
+//     */
+//    public function aggregate(string $field): array
+//    {
+//        return $this->MongoClient->database($this->db)->collection($this->collection)->aggregate([
+//            [
+//                '$group' =>
+//                    [
+//                        '_id' => $field,
+//                        'count' => ['$sum' => 1]
+//                    ]
+//            ],
+//        ]);
+//    }
 
     public function commonInsert(array $data)/*: ?ObjectId|array*/
     {
@@ -141,10 +140,9 @@ class MongoDBHandler
      */
     public function commonUpdate(array $where, array $data): int
     {
-        $update = [
-            '$set' => $data
-        ];
-        $UpdateResult = $this->MongoClient->database($this->db)->collection($this->collection)->updateMany(self::formatWhere($where), $update);
+        $where = self::formatWhere($where);
+        $update = ['$set' => $data];
+        $UpdateResult = $this->MongoClient->database($this->db)->collection($this->collection)->updateMany($where, $update);
         return $UpdateResult->getModifiedCount();
     }
 
@@ -178,6 +176,25 @@ class MongoDBHandler
             }
         }
         return $formatWhere;
+    }
+
+    public static function formatOption(array $select, array $order): array
+    {
+        $option = [];
+        if($select){
+            $id = false;
+            foreach ($select as $unitField){
+                if($unitField === '_id') $id = true;
+                $option['projection'/*聲明需返回的字段*/][$unitField] = 1;//1表示返回
+            }
+            if(!$id) $option['projection']['_id'] = 0;//因爲_id默認返回
+        }
+        if($order){
+            foreach ($order as $unitField => $unitSequence){
+                $option['sort'][$unitField] = ($unitSequence === 'ASC') ? 1/*正序*/ : -1/*倒敘*/;
+            }
+        }
+        return $option;
     }
 
 }
