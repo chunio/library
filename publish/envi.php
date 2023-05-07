72722a471726a3efdd5ecc4fbac2f672c57aa0da
zengweitao <zengweitao@msn.com>
2023-05-07 14:12:49 +0800

    init

--- publish/envi.php
+++ publish/envi.php
@@ -3,10 +3,9 @@
 declare(strict_types=1);
 
 return [
+    //handler[START]
     'traceHandlerStatus' => 1,//是否開啟鏈路跟蹤
     'traceHandlerSync2mongodb' => 1,//是否將輸出同步至mongodb
     'monologHandlerJsonEncodeStatus' => 1,//是否單行，值：0否，1是
     'monologHandlerOutput' => 1,//是否輸出至控制台，值：0否，1是
-    //-----
-    'temp20230328' => 'zengweitao2',
 ];
